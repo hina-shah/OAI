d7ecfba9a7f298064911277996fdc13e5ce78ce0
Hina Shah <hinajoshi1084@gmail.com>
2022-04-13 13:34:14 -0400

    ENH: Changing training script to not need an additional input path

--- python/src/main_training.sh
+++ python/src/main_training.sh
@@ -70,7 +70,12 @@ nbr_features=$((`head -1 Data.csv | sed 's/[^,]//g' | wc -m` -1))
 
 # echo ${inputfile}
 
-python3 ${src_folder}/Step0_AddTrainingData.py ${inputfile} --file ${Data}
+if [ -z $inputfile ];
+then
+	echo " - Missing inputfile, will not append data"
+else
+	python3 ${src_folder}/Step0_AddTrainingData.py ${inputfile} --file ${Data}
+fi
 python3 ${src_folder}/Step0_InterractionFile.py ${Data} -o interactions.csv
 python3 ${src_folder}/Step0_AUC.py -i interactions.csv -o AUC.csv --first_seed ${seed1} --last_seed ${seed_end} --folds ${nbr_folds}
 python3 ${src_folder}/STAT_circ.py ${Data} -o ${output_folder}/circ.pdf --sort pval
